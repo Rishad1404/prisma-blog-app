1add2e7385549a319047c36549309f7096c51dd8
Rishad1404 <rishadislam2019@gmail.com>
2026-01-04 01:46:46 +0600

    user posting

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-import express from "express";
+import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
-const app=express();
+const app:Application=express();
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use('/posts',postRouter);
 
 app.get("/",(req,res)=>{
     res.send("Hello World!");
 })
 
-app.use('/posts',postRouter);
+
 
 export default app
